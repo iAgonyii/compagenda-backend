df376b385d0f586f3325148af5a89a95bc9b59a2
Teneij,Roy H.B <roy.teneij@student.fontys.nl>
2020-04-21 12:12:21 +0200

    Clean up ignored files

--- buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean package && docker build -t org.example/compagenda .
-docker rm -f compagenda || true && docker run -d -p 8080:8080 -p 4848:4848 --name compagenda org.example/compagenda 
